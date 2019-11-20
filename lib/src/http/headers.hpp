e80515dfe0797a34aa5b46630f36d826fba352fd
Damjan Stanisavljevic <damjan.stanisavljevic@comtrade.com>
2019-11-20 14:27:42 +0100

    [LIB][HEADERS] Created basic class http_headers in namespace http

---
+++ lib/src/http/headers.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <map>
+
+namespace http
+{
+    class http_headers
+    {
+    public:
+        http_headers() = default;
+
+        http_headers & add_header(std::string && name, std::string && field)
+        {
+            headers[name] = field;
+        }
+
+    private:
+        std::map<std::string, std::string> headers;
+    };
+} // namespace http
