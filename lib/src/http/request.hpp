884b9cdca7a860c8e301652b4a81b5e6d34f2fe0
Damjan Stanisavljevic <damjan.stanisavljevic@comtrade.com>
2019-11-21 17:54:46 +0100

    [LIB][REQUEST] Added is_set function to class http_request
    This function checks if all the fields of the object are set if
    the field can be unset (the function calls the function is_set on
    the fields of the object if these fields implemented it).

--- lib/src/http/request.hpp
+++ lib/src/http/request.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
+#include <type_traits>
 #include <utility>
-#include "../../include/json.hpp"
 
+#include "../../include/json.hpp"
 #include "headers.hpp"
 #include "method.hpp"
 #include "uri.hpp"
@@ -19,8 +20,11 @@ namespace http
     {
         http_request() = default;
 
-        http_request(HttpMethod && method, HttpUri && uri, HttpHeaders && headers,
-                     HttpBody && body, HttpVersion && version)
+        http_request(HttpMethod  && method,
+                     HttpUri     && uri,
+                     HttpHeaders && headers,
+                     HttpBody    && body,
+                     HttpVersion && version)
             : method(std::forward<HttpMethod>(method))
             , uri(std::forward<HttpUri>(uri))
             , headers(std::forward<HttpHeaders>(headers))
@@ -35,6 +39,15 @@ namespace http
             return *this;
         }
 
+        bool is_set() const
+        {
+            return is_set_if_exists(method)
+                && is_set_if_exists(uri)
+                && is_set_if_exists(version)
+                && is_set_if_exists(headers)
+                && is_set_if_exists(body);
+        }
+
         HttpMethod  method;
         HttpUri     uri;
         HttpHeaders headers;
@@ -42,25 +55,64 @@ namespace http
         HttpVersion version;
     };
 
+    namespace detail
+    {
+        template<typename T>
+        class has_is_set
+        {
+        private:
+            template<typename U, bool (U::*)() const> struct check;
+            template<typename U> static char func(check<U, &U::is_set> *);
+            template<typename U> static int  func(...);
+        public:
+            typedef has_is_set type;
+            enum { value = sizeof(func<T>(0)) == sizeof(char) };
+        };
+
+        template<typename T>
+        bool is_set_if_exists(T const & o, std::true_type)
+        {
+            return o.is_set();
+        }
+
+        template<typename T>
+        constexpr bool is_set_if_exists(T const &, std::false_type)
+        {
+            return true;
+        }
+
+        template<typename T>
+        bool is_set(T const & o)
+        {
+            return is_set_if_exists(o, has_is_set<typename std::decay_t<T>
+                                                 >::type);
+        }
+    } // namespace detail
+
     template<typename OStream, typename HttpMethod,
                                typename HttpUri,
                                typename HttpBody,
                                typename HttpHeaders,
                                typename HttpVersion>
     OStream & operator<<(OStream & out, http_request<HttpMethod,
-                                                      HttpUri,
-                                                      HttpBody,
-                                                      HttpHeaders,
-                                                      HttpVersion> const & request)
+                                                     HttpUri,
+                                                     HttpBody,
+                                                     HttpHeaders,
+                                                     HttpVersion> const & req)
     {
+        if (!req.is_set())
+            return out;
+
         // Status Line
-        out << request.method << ' ' << request.uri
-                              << ' ' << request.version << "\r\n";
+        out << req.method << ' ' << req.uri
+                          << ' ' << req.version << "\r\n";
 
         // Headers
-        out << request.headers << "\r\n";
+        out << req.headers << "\r\n";
 
         //Body
-        out << request.body;
+        out << req.body;
+        
+        return out;
     }
 } // namespace http
