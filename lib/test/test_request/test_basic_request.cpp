bb40c583a81d9358fac031e04ab4d24e39f379ca
Damjan Stanisavljevic <damjan.stanisavljevic@comtrade.com>
2019-11-27 16:32:16 +0100

    [LIB][TEST] Created folder for testing using gtest library and created some unit tests

--- lib/test/test_request/test_basic_request.cpp
+++ lib/test/test_request/test_basic_request.cpp
@@ -1,15 +1,45 @@
-#include <sstream>
-
-#include "gtest/gtest.h"
 #include "http/request.hpp"
 
-TEST (ParseBasicRequest, Default)
+#include "../misc.hpp"
+
+TEST (BasicRequest, Default)
 {
     http::http_request<> req;
     req.uri.uri = "http://test.com/basic";
-    std::ostringstream stream;
-    stream << req;
-    auto str = stream.str();
+    test_eq(req, "GET http://test.com/basic HTTP/1.1\r\n\r\n");
+}
 
-    ASSERT_STREQ(stream.str().c_str(), "GET http://test.com/basic HTTP/1.1\r\n\r\n");
+TEST (BasicRequest, ReqNotSet)
+{
+    http::http_request<> req;
+    test_eq(req, "");
+}
+
+TEST (BasicRequest, SetMethodEnum)
+{
+    http::http_request<> req;
+    req.method = http::method_type::DELETE;
+    req.uri.uri = "http://test.com/basic";
+    test_eq(req, "DELETE http://test.com/basic HTTP/1.1\r\n\r\n");
+}
+
+TEST (BasicRequest, SetMethodString)
+{
+    http::http_request<> req;
+    req.method = "OPTIONS";
+    req.uri.uri = "http://test.com/basic";
+    test_eq(req, "OPTIONS http://test.com/basic HTTP/1.1\r\n\r\n");
+}
+
+TEST (BasicRequest, WrongVersion)
+{
+    http::http_request<> req;
+    req.version.version = "HTTP/1.10";
+    test_eq(req, "");
+    req.version.version = "";
+    test_eq(req, "");
+    req.version.version = "HTTP/11";
+    test_eq(req, "");
+    req.version.version = "1.1";
+    test_eq(req, "");
 }
