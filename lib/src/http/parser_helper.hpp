7a19e8d9e6c5d4ed85dd3c1776c73358ed5a300a
Damjan Stanisavljevic <damjan.stanisavljevic@comtrade.com>
2019-12-25 17:17:09 +0100

    issue_8: Moved functions used by both parsers in a new header file

---
+++ lib/src/http/parser_helper.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <regex>
+#include <string>
+
+#include <boost/algorithm/string.hpp>
+
+#include "body.hpp"
+#include "headers.hpp"
+#include "version.hpp"
+
+namespace http
+{
+    namespace detail
+    {
+        static std::string get_line(std::string & message)
+        {
+            auto line = message.substr(0, message.find_first_of("\r\n"));
+            message.erase(0, line.size() + 2);
+            return line;
+        }
+
+        static http_body parse_body(std::string const & body)
+        {
+            if (body == "")
+                return http_body();
+            return http_body(body);
+        }
+
+        static http_version parse_version(std::string const & version)
+        {
+            if (!std::regex_match(version,
+                                  std::regex("(HTTP/)([0-9])(.)([0-9])")))
+            {
+                std::string what("Unknown version: " + version);
+                throw std::invalid_argument(what + '\n');
+            }
+
+            return http_version(version);
+        }
+
+        static std::vector<std::string> parse_header(std::string const & line)
+        {
+
+            auto delim = line.find_first_of(":");
+            if (delim == std::string::npos)
+            {
+                std::string what("Could not understand header: " + line);
+                throw std::invalid_argument(what + '\n');
+            }
+
+            auto header_name  = line.substr(0, delim);
+            auto header_field = line.substr(delim + 1);
+
+            boost::algorithm::trim(header_name);
+            boost::algorithm::trim(header_field);
+
+            boost::algorithm::to_lower(header_name);
+
+            return std::vector<std::string>{header_name, header_field};
+        }
+    } // namespace detail
+} // namespace http
--- lib/src/http/request_parser.hpp
+++ lib/src/http/request_parser.hpp
@@ -1,24 +1,12 @@
 #pragma once
 
-#include <regex>
-#include <string>
-#include <boost/algorithm/string.hpp>
-
-#include <iostream>
-
+#include "parser_helper.hpp"
 #include "request.hpp"
 
 namespace http
 {
     namespace detail
     {
-        std::string get_line(std::string & message)
-        {
-            auto line = message.substr(0, message.find_first_of("\r\n"));
-            message.erase(0, line.size() + 2);
-            return line;
-        }
-
         http_method parse_method(std::string const & method)
         {
             if (method == "DELETE")
@@ -49,47 +37,6 @@ namespace http
                 throw std::invalid_argument("The request line is too long.\n");
             
             return request_line;
-        }
-
-        http_body parse_body(std::string const & body)
-        {
-            if (body == "")
-                return http_body();
-            return http_body(body);
-        }
-
-        http_version parse_version(std::string const & version)
-        {
-            if (!std::regex_match(version,
-                                  std::regex("(HTTP/)([0-9])(.)([0-9])")))
-            {
-                std::string what("Unknown version: " + version);
-                throw std::invalid_argument(what + '\n');
-            }
-
-            return http_version(version);
-        }
-
-        std::vector<std::string> parse_header(std::string const & line)
-        {
-
-            auto delim = line.find_first_of(":");
-            if (delim == std::string::npos)
-            {
-                std::string what("Could not understand header: " + line);
-                throw std::invalid_argument(what + '\n');
-            }
-
-            auto header_name  = line.substr(0, delim);
-            auto header_field = line.substr(delim + 1);
-
-            boost::algorithm::trim(header_name);
-            boost::algorithm::trim(header_field);
-
-            boost::algorithm::to_lower(header_name);
-            boost::algorithm::to_lower(header_field);
-
-            return std::vector<std::string>{header_name, header_field};
         }
     } // namespace detail
 
