35842519257b9bfb3261cace2bf7f43ca572987a
Damjan Stanisavljevic <damjan.stanisavljevic@comtrade.com>
2019-12-25 17:20:47 +0100

    issue_8: Modified helper file for the tests to be more generic

--- lib/test/test_utility.hpp
+++ lib/test/test_utility.hpp
@@ -12,3 +12,18 @@ void test_eq(Request const & req, std::string const & expected)
     stream << req;
     ASSERT_EQ(stream.str(), expected);
 }
+
+template <typename Parser>
+void test_exception(Parser parser,
+                    std::string const & obj,
+                    std::string const & expected)
+{
+    try {
+        parser(obj);
+        FAIL() << "Expected std::invalid_argument";
+    } catch (std::exception const & e) {
+        ASSERT_EQ(e.what(), expected);
+    } catch (...) {
+        FAIL() << "Expected std::invalid_argument";
+    }
+}
